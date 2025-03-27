868296a0c9679d442db6a33ba3006ea7f83ee3e8
Gabriel Brum <gbrb1@hotmail.com>
2025-03-26 22:28:41 -0300

    documentando as rotas e fazendo ajustes nas queries do repository

--- backend/app/repositories/company_repo.py
+++ backend/app/repositories/company_repo.py
@@ -3,13 +3,12 @@ from app.models.company import Company
 from sqlalchemy.exc import SQLAlchemyError
 
 
-def get_companies_by_name(db: Session, name: str, limit: int = 50):
+def get_companies_by_name(db: Session, name: str):
     try:
         return (
             db.query(Company)
             .filter(Company.nome_fantasia.ilike(f"%{name}%"))
             .order_by(Company.nome_fantasia)
-            .limit(limit)
             .all()
         )
     except SQLAlchemyError as e:
@@ -17,18 +16,16 @@ def get_companies_by_name(db: Session, name: str, limit: int = 50):
 
 def get_all_companies(db: Session):
     try:
-        return db.query(Company).order_by(Company.nome_fantasia).limit(50).all()
+        return db.query(Company).all()
     except SQLAlchemyError as e:
         return {"error": str(e)}
 
 
-def get_by_cnpj(db: Session, cnpj: str, limit: int = 50):
+def get_by_cnpj(db: Session, cnpj: str):
     try:
         return (
             db.query(Company)
             .filter(Company.cnpj.ilike(f"{cnpj}%")) 
-            .order_by(Company.cnpj)
-            .limit(limit)
             .all()
         )
     except SQLAlchemyError as e:
--- backend/app/routes/company_routes.py
+++ backend/app/routes/company_routes.py
@@ -3,17 +3,31 @@ from sqlalchemy.orm import Session
 from app.database import get_db
 from app.services.company_service import search_companies, get_all as get_all_companies, get_by_cnpj
 
-router = APIRouter(prefix="/companies", tags=["Companies"])
+router = APIRouter(prefix="/companies/api", tags=["Companies"])
 
 @router.get("/search")
 def search(name: str, db: Session = Depends(get_db)):
+    """
+    Endpoint para buscar as empresas pelo nome, para utilizar no browser, coloque
+    o parâmetro 'name' no final, exemplo:
+    http://127.0.0.1:8000/companies/api/search?name=AMERON
+    """
     return search_companies(db, name)
 
 
 @router.get("/getall")
 def get_all(db: Session = Depends(get_db)):
+    """
+    Apenas um getall, para buscar todas as empresas em banco,
+    acesse em companies/api/getall, sem parâmetros
+    """
     return get_all_companies(db)
 
 @router.get("/getbycnpj")
 def get_cnpj(cnpj: str, db: Session = Depends(get_db)):
+    """
+    Endpoint para buscar as empresas pelo CNPJ, para utilizar no browser, coloque
+    o parâmetro 'cnpj' no final, exemplo:
+    http://127.0.0.1:8000/companies/api/getbycnpj?cnpj=84638345000165
+    """
     return get_by_cnpj(db, cnpj)
